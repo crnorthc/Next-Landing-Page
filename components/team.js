8e248f031d6fbe29486880a1a893fafd4b08c216
Matthew <matthew.j.klimek@gmail.com>
2021-11-01 16:51:22 -0400

    pipes and team

--- components/team.js
+++ components/team.js
@@ -3,42 +3,80 @@ import React from "react";
 function team() {
  return (
   <section
-   className='bg-dark pt-20 sm:pt-40 pb-10 sm:pb-20 px-5 sm:px-10 over-tokens h-screen relative'
+   className='bg-dark pt-20 sm:pt-40  px-5 sm:px-10 over-tokens relative'
    id='team'
   >
    {/* left pipe */}
    <div className='hidden xl:block absolute bottom-0 left-0 h-full w-20 bg-medium border-t-8 border-t-primary rounded-bl-2xl'></div>
-   <div className='hidden xl:block absolute top-0 bottom-10 left-8 w-3  bg-light pipe-shine '></div>
 
    {/* right pipe */}
-   <div className='hidden xl:block absolute bottom-0 right-0 h-full w-20 bg-medium border-t-8 border-t-primary rounded-bl-2xl'></div>
-   <div className='hidden xl:block absolute top-0 bottom-10 right-8 w-3  bg-light pipe-shine '></div>
+   <div className='hidden xl:block absolute top-0 bottom-right-team right-0 w-20 bg-medium border-t-8 border-t-primary rounded-br-2xl'></div>
+   {/* <div className=''>
+    <h2 className='text-light text-5xl sm:text-6xl text-center pb-10 glory'>
+     Team
+    </h2>
+   </div> */}
 
-   {/* Robard */}
-   <div className='absolute bottom-20 left-5 xl:left-40 w-28 h-16 rounded-t-xl bg-medium '></div>
-   <div className='absolute bottom-32 left-5 xl:left-40 w-28 h-4 rounded-full bg-black border-4 border-primary  pipe-primary-shadow'></div>
+   <div className='relative h-96 w-3/4 ml-auto'>
+    <h3 className='text-light text-4xl sm:text-5xl text-center pl-20 glory'>
+     Core Team
+    </h3>
+    {/* Bottom pipe1 */}
+    <div className='absolute bottom-0 left-0 right-0 h-20 bg-medium  border-l-8 border-l-black rounded-l-md'></div>
+    <div className='absolute bottom-8 right-2 left-4  h-3 bg-light pipe-shine'></div>
 
-   <img
-    className='absolute bottom-32 pb-1 left-9 xl:left-44 w-20'
-    src='/robard-head-left-3D.png'
-    alt=''
-   />
+    {/* Caleb */}
 
-   {/* Bottom pipe1 */}
-   <div className='absolute bottom-1/2 left-0 xl:left-1/4 right-0 h-20 bg-medium  rounded-tr-2xl'></div>
-   <div className='absolute bottom-1/2 right-8 left-0 xl:left-20  h-3 bg-light pipe-shine'></div>
-   <div className='hidden xl:block absolute bottom-1/2 left-10 w-10 h-3 bg-light pipe-shine'></div>
-   <div className='hidden xl:block absolute bottom-1/2 right-8 h-10 w-3 bg-light pipe-shine'></div>
+    <div className='absolute bottom-20 left-1/3 w-28 h-16 rounded-t-xl bg-medium '></div>
+    <div className='absolute bottom-32 left-1/3 w-28 h-4 rounded-full bg-black border-4 border-primary  pipe-primary-shadow'></div>
 
-   {/* Bottom pipe2 */}
-   <div className='absolute bottom-1/4 left-0 xl:left-20 xl:right-1/4 h-20 bg-medium '></div>
-   <div className='absolute bottom-1/4 right-8 left-0 xl:left-20  h-3 bg-light pipe-shine'></div>
-   <div className='hidden xl:block absolute bottom-1/4 left-10 w-10 h-3 bg-light pipe-shine'></div>
-   <div className='hidden xl:block absolute bottom-1/4 right-8 h-10 w-3 bg-light pipe-shine'></div>
+    <img
+     className='absolute bottom-40 caleb-head w-20'
+     src='/Caleb.png'
+     alt=''
+    />
 
-   <h2 className='text-light text-5xl sm:text-6xl text-center pb-10 glory'>
-    Team
-   </h2>
+    {/* Matt */}
+
+    <div className='absolute bottom-20 left-2/3 w-28 h-16 rounded-t-xl bg-medium '></div>
+    <div className='absolute bottom-32 left-2/3 w-28 h-4 rounded-full bg-black border-4 border-primary  pipe-primary-shadow'></div>
+
+    <img className='absolute bottom-40 matt-head w-20' src='/Matt.png' alt='' />
+   </div>
+
+   <div className='relative h-96 mb-96 w-3/4 mr-auto mt-40'>
+    <h3 className='text-light text-4xl sm:text-5xl text-center pl-20 glory'>
+     Advisors
+    </h3>
+    {/* Bottom pipe1 */}
+    <div className='absolute bottom-0 left-0 right-0 h-20 bg-medium  border-r-8 border-r-black rounded-r-md'></div>
+    <div className='absolute bottom-8 right-4 left-2  h-3 bg-light pipe-shine'></div>
+
+    {/* Caleb */}
+
+    <div className='absolute bottom-20 left-1/3 w-28 h-16 rounded-t-xl bg-medium '></div>
+    <div className='absolute bottom-32 left-1/3 w-28 h-4 rounded-full bg-black border-4 border-primary  pipe-primary-shadow'></div>
+
+    <img
+     className='absolute bottom-40 caleb-head w-20'
+     src='/Caleb.png'
+     alt=''
+    />
+
+    {/* Matt */}
+
+    <div className='absolute bottom-20 left-2/3 w-28 h-16 rounded-t-xl bg-medium '></div>
+    <div className='absolute bottom-32 left-2/3 w-28 h-4 rounded-full bg-black border-4 border-primary  pipe-primary-shadow'></div>
+
+    <img className='absolute bottom-40 matt-head w-20' src='/Matt.png' alt='' />
+   </div>
+
+   {/* white blur left */}
+   <div className='hidden xl:block absolute top-0 bottom-10 left-8 w-3 bg-light pipe-shine '></div>
+
+   {/* white blur right */}
+   <div className='hidden xl:block absolute top-0 bottom-right-team-shine right-8 w-3  bg-light pipe-shine '></div>
+
    {/* <div className='flex flex-col items-center justify-center lg:items-start lg:flex-row'>
     <div className='flex-1 p-5 flex flex-col items-center justify-center'>
      <img className='w-2/4 lg:w-1/3 rounded-md' src='profile.png' alt='' />
